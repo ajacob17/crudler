598c22acd6d065327d32c8cb996100165f819dd6
ajacob17 <157540368+ajacob17@users.noreply.github.com>
2024-10-11 03:52:04 +0100

    Completed second video changes (#1)

---
+++ src/components/screens/ModuleModifyScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text } from 'react-native';
+import Screen from '../layout/Screen';
+
+export const ModuleModifyScreen = () => {
+  return (
+    <Screen>
+
+
+      
+      <Text>Modify</Text>
+
+
+
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ModuleModifyScreen;
